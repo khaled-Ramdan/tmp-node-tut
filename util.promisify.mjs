f055e31288cbb0721ccc0e7bbabdde42386dc240
Khaled <Khaled195341@feng.bu.edu.eg>
2022-10-14 22:03:21 +0200

    util.promisify

---
+++ util.promisify.mjs
@@ -0,0 +1,20 @@
+import util from "util";
+import { readFile, writeFile } from "fs";
+const readfilepromise = util.promisify(readFile);
+const writeFilepromise = util.promisify(writeFile);
+async function start() {
+	try {
+		const fr = await readfilepromise(
+			new URL("value.txt", import.meta.url),
+			"utf-8"
+		);
+		const sc = await readfilepromise(
+			new URL("textfile.txt", import.meta.url),
+			"utf-8"
+		);
+		console.log(fr, sc);
+	} catch (err) {
+		console.log(err);
+	}
+}
+start();
